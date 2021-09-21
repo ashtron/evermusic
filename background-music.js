ce3b8cce0115bdd64c6ed3d16d6047180aa8ffc6
ashtron <ashton.playsted@protonmail.com>
2021-09-21 13:14:52 -0700

    Spotify plays when no video is playing in Firefox

--- background-music.js
+++ background-music.js
@@ -1,19 +1,20 @@
-const { execSync } = require('child_process')
+const { execSync, exec } = require('child_process')
 
 const spotifyPlay = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play"
 const spotifyPause = "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause"
 
 let run = function() {
-    var soundDeviceData = execSync("pacmd list-sink-inputs").toString().split("\t")
+    let soundDeviceData = execSync("pacmd list-sink-inputs").toString().split("\t")
+    let videoPlaying = false
     
     for (let i = 0; i < soundDeviceData.length; i++) {
-        if (soundDeviceData[i].includes("client") && soundDeviceData[i].includes("Firefox")) {
-            console.log(soundDeviceData[i - 12])
-    
+        if (soundDeviceData[i].includes("client") && soundDeviceData[i].includes("Firefox")) {    
             if (soundDeviceData[i - 12].includes("RUNNING"))
-                execSync(spotifyPause)
+                videoPlaying = true
         }
     }
+
+    videoPlaying ? execSync(spotifyPause) : execSync(spotifyPlay)
 }
 
 var intervalID = setInterval(run, 100)
